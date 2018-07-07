d88cfde980f8505776d05d693da029afa0639f8c
Alan Garny <agarny@hellix.com>
2018-07-07 08:51:28 +0200

    Upgraded to version 2.10.7.

--- Python/configure-old.py
+++ Python/configure-old.py
@@ -77,7 +77,7 @@ def create_optparser():
         setattr(parser.values, option.dest, os.path.abspath(value))
 
     p = optparse.OptionParser(usage="python %prog [options]",
-            version="2.10.6")
+            version="2.10.7")
 
     p.add_option("-a", "--apidir", action="callback", default=None,
             type="string", metavar="DIR", dest="qscidir",
@@ -177,8 +177,8 @@ def check_qscintilla():
             # Because we include the Python bindings with the C++ code we can
             # reasonably force the same version to be used and not bother about
             # versioning.
-            if sciversstr != "2.10.6":
-                sipconfig.error("QScintilla %s is being used but the Python bindings 2.10.6 are being built.  Please use matching versions." % sciversstr)
+            if sciversstr != "2.10.7":
+                sipconfig.error("QScintilla %s is being used but the Python bindings 2.10.7 are being built.  Please use matching versions." % sciversstr)
 
             sipconfig.inform("QScintilla %s is being used." % sciversstr)
         else:
