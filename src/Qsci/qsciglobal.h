750b013e9e7dc940ca5858e00a13067dde836c38
Alan Garny <agarny@hellix.com>
2018-12-22 09:46:17 +1300

    Make sure that QScintilla gets built as we want for OpenCOR.

--- src/Qsci/qsciglobal.h
+++ src/Qsci/qsciglobal.h
@@ -34,15 +34,11 @@
 #endif
 
 
-// Define QSCINTILLA_MAKE_DLL to create a QScintilla shared library, or
-// define QSCINTILLA_DLL to link against a QScintilla shared library, or define
-// neither to either build or link against a static QScintilla library.
-#if defined(QSCINTILLA_DLL)
-#define QSCINTILLA_EXPORT       Q_DECL_IMPORT
-#elif defined(QSCINTILLA_MAKE_DLL)
-#define QSCINTILLA_EXPORT       Q_DECL_EXPORT
+// Define QSCINTILLA_MAKE_DLL to create a QScintilla shared library.
+#ifdef QSCINTILLA_MAKE_DLL
+    #define QSCINTILLA_EXPORT Q_DECL_EXPORT
 #else
-#define QSCINTILLA_EXPORT
+    #define QSCINTILLA_EXPORT Q_DECL_IMPORT
 #endif
 
 
